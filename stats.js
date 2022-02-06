f8cc85b3c71dec4027148e2208a3b1cb1d42c617
Spyboy <itisshubhamkumar@gmail.com>
2022-02-06 18:18:35 +0530

    .

--- stats.js
+++ stats.js
@@ -7,7 +7,13 @@ function fetchData() {
 		console.log(data.data[0].users);
 		document.querySelector('#user-count').innerHTML = `<p>${data.data[0].users}+</p>`
 		console.log(data.data[0].active);
-		document.querySelector('#active-count').innerHTML = `<p>${data.data[0].active}+</p>`
+		document.querySelector('#active').innerHTML = `<p>${data.data[0].active}+</p>`
+		console.log(data.data[0].commands);
+		document.querySelector('#commands').innerHTML = `<p>${data.data[0].commands}+</p>`
+		console.log(data.data[0].cpuload);
+		document.querySelector('#cpuload').innerHTML = `<p>${data.data[0].cpuload}%</p>`
+		console.log(data.data[0].memload);
+		document.querySelector('#memload').innerHTML = `<p>${data.data[0].memload}%</p>`
 	}).catch(error => {
 		console.log(error);
 	});
