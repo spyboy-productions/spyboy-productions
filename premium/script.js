dc6ab7b2c74449776d48557b90c8650b6f605028
FlamptX <dino.shark2006@gmail.com>
2022-05-22 18:48:45 +0200

    pro move

--- premium/script.js
+++ premium/script.js
@@ -22,12 +22,7 @@ if (discordUsername != null && discordId != null) {
 }
 
 loginBtn.addEventListener('click', () => {
-    fetch("https://discord.com/api/oauth2/authorize?client_id=877644741339144244&redirect_uri=https://spyoweb.herokuapp.com/redirect&response_type=code&scope=identify", {
-      method: "GET",
-      mode: 'no-cors',
-    }).then(res => {
-      console.log("Request complete! response:", res);
-    });
+    window.location.href = 'https://discord.com/api/oauth2/authorize?client_id=877644741339144244&redirect_uri=https://spyoweb.herokuapp.com/redirect&response_type=code&scope=identify'
 });
 checkoutBtn.addEventListener('click', () => {
     fetch("https://spyoweb.herokuapp.com/create-checkout-session", {
