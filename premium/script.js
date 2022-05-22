380215a9afa57477df2aaa9976a4fe50a79fd6d0
FlamptX <dino.shark2006@gmail.com>
2022-05-22 17:56:09 +0200

    testing

--- premium/script.js
+++ premium/script.js
@@ -24,11 +24,12 @@ if (discordUsername != null && discordId != null) {
 loginBtn.addEventListener('click', () => {
     fetch("https://spyoweb.herokuapp.com/discord-login", {
       method: "GET",
+      mode: 'cors',
       headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Credentials': 'true'
-          //'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
-          //'Access-Control-Allow-Headers': 'access-control-allow-credentials,access-control-allow-headers,access-control-allow-methods,access-control-allow-origin'
+          'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+          'Access-Control-Allow-Headers': '*' // access-control-allow-credentials,access-control-allow-headers,access-control-allow-methods,access-control-allow-origin
       }
     }).then(res => {
       console.log("Request complete! response:", res);
