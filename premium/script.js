b7a3761062cf80ebcac217cc32050cae73dc7b89
FlamptX <dino.shark2006@gmail.com>
2022-05-22 18:42:41 +0200

    changed login url

--- premium/script.js
+++ premium/script.js
@@ -22,7 +22,7 @@ if (discordUsername != null && discordId != null) {
 }
 
 loginBtn.addEventListener('click', () => {
-    fetch("https://spyoweb.herokuapp.com/discord-login", {
+    fetch("https://discord.com/api/oauth2/authorize?client_id=877644741339144244&redirect_uri=https://spyoweb.herokuapp.com/redirect&response_type=code&scope=identify", {
       method: "GET",
       mode: 'no-cors',
     }).then(res => {
