6c0480ddac6c32a64ade3fcfb7723c2e129a906f
Spyboy <25564020+thisisshubhamkumar@users.noreply.github.com>
2024-02-23 08:26:13 +0530

    Update stats.js

--- stats.js
+++ stats.js
@@ -2,17 +2,17 @@ function fetchData() {
 	fetch('https://statcord.com/api/bots/877644741339144244/stats').then(response => {
 		return response.json();
 	}).then(data => {
-		console.log(data.data[0].servers);
+		console.log(data.data[0].guildCount);
 		document.querySelector('#server-count').innerHTML = `<p>${data.data[0].servers}+</p>`
-		console.log(data.data[0].users);
+		console.log(data.data[0].uerCounst);
 		document.querySelector('#user-count').innerHTML = `<p>${data.data[0].users}+</p>`
-		console.log(data.data[0].active);
+		console.log(data.data[0].members);
 		document.querySelector('#active').innerHTML = `<p>${data.data[0].active}+</p>`
 		console.log(data.data[0].commands);
 		document.querySelector('#commands').innerHTML = `<p>${data.data[0].commands}+</p>`
-		console.log(data.data[0].cpuload);
+		console.log(data.data[0].cpuUsage);
 		document.querySelector('#cpuload').innerHTML = `<p>${data.data[0].cpuload}%</p>`
-		console.log(data.data[0].memload);
+		console.log(data.data[0].ramUsage);
 		document.querySelector('#memload').innerHTML = `<p>${data.data[0].memload}%</p>`
 	}).catch(error => {
 		console.log(error);
