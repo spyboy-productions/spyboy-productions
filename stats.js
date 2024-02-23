51061b1ae1483856b9163cafd5a49adc032d6e58
Spyboy <25564020+thisisshubhamkumar@users.noreply.github.com>
2024-02-23 08:57:56 +0530

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,22 +1,16 @@
 function fetchData() {
-	fetch('https://statcord.com/api/bots/877644741339144244/stats').then(response => {
-		return response.json();
-	}).then(data => {
-		console.log(data.data[0].guildCount);
-		document.querySelector('#server-count').innerHTML = `<p>${data.data[0].guildCount}+</p>`
-		console.log(data.data[0].userCount);
-		document.querySelector('#user-count').innerHTML = `<p>${data.data[0].userCount}+</p>`
-		console.log(data.data[0].members);
-		document.querySelector('#active').innerHTML = `<p>${data.data[0].members}+</p>`
-		console.log(data.data[0].shardCount);
-		document.querySelector('#commands').innerHTML = `<p>${data.data[0].shardCount}+</p>`
-		console.log(data.data[0].cpuUsage);
-		document.querySelector('#cpuload').innerHTML = `<p>${data.data[0].cpuUsage}%</p>`
-		console.log(data.data[0].ramUsage);
-		document.querySelector('#memload').innerHTML = `<p>${data.data[0].ramUsage}%</p>`
-	}).catch(error => {
-		console.log(error);
-	});
+  fetch('https://statcord.com/api/bots/877644741339144244/stats')
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector('#server-count').innerHTML = `<p>${data.guildCount}+</p>`;
+      // Update other elements similarly:
+      document.querySelector('#user-count').innerHTML = `<p>${data.userCount}+</p>`;
+      document.querySelector('#active').innerHTML = `<p>${data.members}+</p>`;
+      document.querySelector('#commands').innerHTML = `<p>${data.shardCount}+</p>`;
+      document.querySelector('#cpuload').innerHTML = `<p>${data.cpuUsage}%</p>`;
+      document.querySelector('#memload').innerHTML = `<p>${data.ramUsage}%</p>`;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
 }
-
-fetchData();
