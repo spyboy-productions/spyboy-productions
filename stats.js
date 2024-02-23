cf56eccf9d9743f5f2440ec6709d5ce866d8831d
Spyboy <25564020+thisisshubhamkumar@users.noreply.github.com>
2024-02-23 22:57:39 +0530

    Update stats.js

--- stats.js
+++ stats.js
@@ -2,12 +2,23 @@ function fetchData() {
     fetch('https://statcord.com/api/bots/877644741339144244/stats')
         .then(response => response.json())
         .then(data => {
-            const shards = data.mainStats.stats.find(e => e.name === 'Shards').data.datasets[0].data.slice(-1)[0];
-            const users = data.mainStats.stats.find(e => e.name === 'User Count').data.datasets[0].data.slice(-1)[0];
-            const cpu = data.mainStats.stats.find(e => e.name === 'CPU Usage').data.datasets[0].data.slice(-1)[0];
-            const ram = data.mainStats.stats.find(e => e.name === 'Ram Usage').data.datasets[0].data.slice(-1)[0];
-            const guilds = data.mainStats.stats.find(e => e.name === 'Guild Growth').data.datasets[0].data.slice(-1)[0];
-            const members = data.mainStats.stats.find(e => e.name === 'Members').data.datasets[0].data.slice(-1)[0];
+            const shardsData = data.mainStats.stats.find(e => e.name === 'Shards').data.datasets[0].data;
+            const shards = shardsData[shardsData.length - 1];
+
+            const userData = data.mainStats.stats.find(e => e.name === 'User Count').data.datasets[0].data;
+            const users = userData[userData.length - 1];
+
+            const cpuData = data.mainStats.stats.find(e => e.name === 'CPU Usage').data.datasets[0].data;
+            const cpu = cpuData[cpuData.length - 1];
+
+            const ramData = data.mainStats.stats.find(e => e.name === 'Ram Usage').data.datasets[0].data;
+            const ram = ramData[ramData.length - 1];
+
+            const guildData = data.mainStats.stats.find(e => e.name === 'Guild Growth').data.datasets[0].data;
+            const guilds = guildData[guildData.length - 1];
+
+            const membersData = data.mainStats.stats.find(e => e.name === 'Members').data.datasets[0].data;
+            const members = membersData[membersData.length - 1];
 
             console.log(users);
             document.querySelector('#user-count').innerHTML = `<p>${users}+</p>`;
@@ -23,6 +34,9 @@ function fetchData() {
 
             console.log(ram);
             document.querySelector('#memload').innerHTML = `<p>${ram}%</p>`;
+
+            console.log(guilds);
+            document.querySelector('#server-count').innerHTML = `<p>${guilds}+</p>`;
         })
         .catch(error => {
             console.log(error);
